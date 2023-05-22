733b71bf9f484bb47c118f2a208a8e1e9ef1d2b3
guilhermeabell <gpeuvis@gmail.com>
2023-05-22 11:31:07 -0300

    WIP

---
+++ app/api/register/route.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt'
+
+import prisma from '@/app/libs/prismadb'
+import { NextResponse } from 'next/server'
+
+export async function POST(
+  request: Request,
+
+) {
+  const body = await request.json()
+  const { email, name, password } = body
+
+  if (!email || !name || !password) {
+    return new NextResponse('Missing info', { status: 400 })
+  }
+}
